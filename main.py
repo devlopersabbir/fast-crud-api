f7c375bc99fabbc6526769742fbf0d1dc916fa7f
devlopersabbir <devlopersabbir@gmail.com>
2023-10-08 06:11:19 +0600

    � routes �

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from fastapi import FastAPI
+from routes.route import router
 
 app = FastAPI()
 
-
-uri = ""
-
-# Create a new client and connect to the server
-client = MongoClient(uri)
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("DB connection successfully")
-except Exception as e:
-    print("Fail to connect DB")
+app.include_router(router)
--- routes/route.py
+++ routes/route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from models.users import User
 from configs.database import collectionName
 from schema.schemas import index
@@ -13,3 +13,24 @@ router = APIRouter()
 async def getAllUsers():
     users = index(collectionName.find())
     return users
+
+# create new user
+
+
+@router.post("/")
+async def createUser(user: User):
+    try:
+        print(user.email)
+        print("================")
+        collectionName.insert_one(dict(user))
+        return {
+            "message": "user created successfully!",
+            "user": user
+        }
+    except Exception as error:
+        print(error)
+        return {
+            "message": "Something went wrong!",
+            "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+            "error": error
+        }
